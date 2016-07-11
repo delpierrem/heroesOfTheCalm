dc346419762198730d6daa87a7abb5357cb47c76
Spawnk <delpierre.michel@outlook.fr>
2016-07-11 16:55:56 +0200

    clean repository and fix bug

---
+++ script/battle.js
@@ -0,0 +1,214 @@
+var player1id = localStorage.getItem("player1id");
+var player2id = localStorage.getItem("player2id");
+var player2Name = localStorage.getItem("player2Name");
+var player1Name = localStorage.getItem("player1Name");
+
+document.getElementById("player2img").innerHTML = "<img src=\"img/" + player2id + ".jpg\" alt=\"...\">";
+document.getElementById("player1img").innerHTML = "<img src=\"img/" + player1id + ".jpg\" alt=\"...\">";
+
+
+var Player = function() {
+    this.pseudo = 'mystery';
+    this.life = 100;
+    this.dmg = 0;
+    this.heal = 0;
+    this.boost = 0;
+    this.shield = 0;
+    this.itemDps = 0;
+    this.itemHeal = 0;
+    this.itemShield = 0;
+
+};
+
+var player1 = new Player();
+player1.pseudo = player1Name;
+
+var player2 = new Player();
+player2.pseudo = player2Name;
+
+var canPlay;
+
+document.getElementById("heroes1").innerHTML = player1.pseudo;
+document.getElementById("life1").innerHTML = player1.life;
+document.getElementById("heroes2").innerHTML = player2.pseudo;
+document.getElementById("life2").innerHTML = player2.life;
+// need to correct heal with canPlay
+var healplayer2 = function() {
+    if (player2.life <= 0 || player2.life >= 90) {
+        window.alert("U can't use heal now !");
+    } else {
+        player2.heal = Math.floor(Math.random() * 10) + 1;
+        var hp = player2.itemHeal + player2.life + player2.heal;
+        player2.life = hp;
+        document.getElementById("life2").innerHTML = hp;
+        document.getElementById("round").innerHTML = "player1 it's your round";
+        canPlay++;
+    }
+};
+
+var healplayer1 = function() {
+    if (player1.life <= 0 || player1.life >= 90) {
+        window.alert("U can't use heal now !");
+    } else {
+        player1.heal = Math.floor(Math.random() * 10) + 1;
+        var hp2 = player1.itemHeal + player1.life + player1.heal;
+        player1.life = hp2;
+        document.getElementById("life1").innerHTML = hp2;
+        document.getElementById("round").innerHTML = "player2 it's your round";
+        canPlay--;
+    }
+};
+
+var attackplayer1 = function() {
+    if (player2.life > 0 && player1.life > 0 && canPlay == 2) {
+        player1.dmg = player1.itemDps + player1.boost + Math.floor(Math.random() * 10) + 1;
+        document.getElementById("dmg1").innerHTML = player1.dmg + " DPS";
+        // document.getElementById("dmg1").style.fontSize = '100px';
+        var hp = (player2.life + player2.shield) - player1.dmg;
+        player2.life = hp;
+        document.getElementById("life2").innerHTML = hp;
+        document.getElementById("round").innerHTML = "player2 it's your round";
+        canPlay--;
+        if (player2.life <= 0) {
+            window.alert("player1 win");
+            document.getElementById("life2").innerHTML = "0";
+        }
+    } else if (player2.life <= 0) {
+        window.alert("Fuck u little bitch !");
+    }
+};
+
+var attackplayer2 = function() {
+    if (player1.life > 0 && player2.life > 0 && canPlay == 1) {
+        player2.dmg = player2.itemDps + player2.boost + Math.floor(Math.random() * 10) + 1;
+        document.getElementById("dmg2").innerHTML = player2.dmg + " DPS";
+        var hp2 = (player1.life + player1.shield) - player2.dmg;
+        player1.life = hp2;
+        document.getElementById("life1").innerHTML = hp2;
+        document.getElementById("round").innerHTML = "player1 it's your round";
+        canPlay++;
+        if (player1.life <= 0) {
+            window.alert("player2 win");
+            document.getElementById("life1").innerHTML = "0";
+        }
+    } else if (player2.life <= 0) {
+        window.alert("Fuck u little bitch !");
+    }
+};
+
+var boostplayer2 = function() {
+    if (player1.life > 0 && player2.life > 0 && canPlay == 1) {
+        var boost = player2.boost + Math.floor(Math.random() * 10) + 1;
+        player2.boost = boost;
+        document.getElementById("round").innerHTML = "player1 it's your round";
+        canPlay++;
+    }
+};
+
+var boostplayer1 = function() {
+    if (player2.life > 0 && player1.life > 0 && canPlay == 2) {
+        var boosted = player1.boost + Math.floor(Math.random() * 10) + 1;
+        player1.boost = boosted;
+        document.getElementById("round").innerHTML = "player2 it's your round";
+        canPlay--;
+    }
+};
+
+var shieldplayer2 = function() {
+    if (player2.life > 0 && player1.life > 0 && canPlay == 1) {
+        var shield = player2.shield + Math.floor(Math.random() * 5) + 1;
+        player2.shield = player2.itemShield + shield;
+        document.getElementById("round").innerHTML = "player1 it's your round";
+        document.getElementById("shield1").innerHTML = player2.shield;
+        canPlay++;
+    }
+};
+
+var shieldplayer1 = function() {
+    if (player1.life > 0 && player2.life > 0 && canPlay == 2) {
+        var shield1 = player1.shield + Math.floor(Math.random() * 5) + 1;
+        player1.shield = player1.itemShield + shield1;
+        document.getElementById("round").innerHTML = "player2 it's your round";
+        document.getElementById("shield").innerHTML = player1.shield;
+        canPlay--;
+    }
+};
+
+// function for starter item
+function addDpsplayer2() {
+    var dpsAddPlayer = 5;
+    player2.itemDps = dpsAddPlayer;
+    document.getElementById("item1").innerHTML = '<p class=\"btn btn-danger btn-lg btn-block\">+5 dps</p>';
+}
+
+function addDpsplayer1() {
+    var dpsAddPlayer = 5;
+    player1.itemDps = dpsAddPlayer;
+    document.getElementById("item2").innerHTML = '<p class=\"btn btn-danger btn-lg btn-block\">+5 dps</p>';
+}
+// Item add Heal boost at player
+function addHealplayer1() {
+    var healAddPlayer = 5;
+    player1.itemHeal = healAddPlayer;
+    document.getElementById("item2").innerHTML = '<p class=\"btn btn-primary btn-lg btn-block\">+5 Heal</p>';
+}
+
+function addHealplayer2() {
+    var healAddPlayer = 5;
+    player2.itemHeal = healAddPlayer;
+    document.getElementById("item1").innerHTML = '<p class=\"btn btn-primary btn-lg btn-block\">+5 Heal</p>';
+}
+// Item add life at player
+function addLifeplayer1() {
+    var lifeAddPlayer = 125;
+    player1.life = lifeAddPlayer;
+    document.getElementById("item2").innerHTML = '<p class="btn btn-success btn-lg btn-block">+25 life</p>';
+    document.getElementById("life1").innerHTML = player1.life;
+}
+
+function addLifeplayer2() {
+    var lifeAddPlayer = 125;
+    player2.life = lifeAddPlayer;
+    document.getElementById("item1").innerHTML = '<p class="btn btn-success btn-lg btn-block">+25 life</p>';
+    document.getElementById("life2").innerHTML = player2.life;
+}
+
+function addShieldplayer1() {
+    var shieldAddPlayer = 3;
+    player1.itemShield = shieldAddPlayer;
+    document.getElementById("item2").innerHTML = '<p class="btn btn-warning btn-lg btn-block">+3 shield</p>';
+    document.getElementById("shield").innerHTML = player1.itemShield;
+}
+
+function addShieldplayer2() {
+    var shieldAddPlayer = 3;
+    player2.itemShield = shieldAddPlayer;
+    document.getElementById("item1").innerHTML = '<p class="btn btn-warning btn-lg btn-block">+3 shield</p>';
+    document.getElementById("shield1").innerHTML = player2.itemShield;
+}
+
+function getcanPlay() {
+    canPlay = Math.floor(Math.random() * 2) + 1;
+    if (canPlay == 1) {
+        document.getElementById("round").innerHTML = "player2 it's your round";
+    } else if (canPlay == 2) {
+        document.getElementById("round").innerHTML = "player1 it's your round";
+    }
+    return canPlay;
+}
+
+var reset = function() {
+    player1.life = 100;
+    player2.life = 100;
+    player2.dmg = 0;
+    player1.dmg = 0;
+    player1.shield = 0;
+    player2.shield = 0;
+    document.getElementById("life1").innerHTML = player1.life;
+    document.getElementById("life2").innerHTML = player2.life;
+    document.getElementById("dmg1").innerHTML = player1.dmg;
+    document.getElementById("dmg2").innerHTML = player2.dmg;
+    document.getElementById("shield").innerHTML = player1.shield;
+    document.getElementById("shield1").innerHTML = player2.shield;
+    getcanPlay();
+};
---
+++ script/select.js
@@ -0,0 +1,61 @@
+var tablepop = ['Michel', 'Quentin', 'Jordan', 'Victor', "Kevin", "Tristan",
+    "Baptiste", "Daniel", "FrançoisF", "Axel", "Antoine", "Bonsergent", "JessiCat",
+    "Jessy", "Julie", "Kylian", "Hao", "Manon", "Paul", "El Pietro", "Raphaë", "Thimoty",
+];
+var i = 0;
+var a = 0;
+var ready = 1;
+var player1;
+var player2;
+
+
+
+function load() {
+    for (i; i < tablepop.length; i++) {
+        document.getElementById("tablepop").innerHTML += "<div class =\"col-sm-6 col-md-2\" id=\"" + i + "\" onclick=\"selectFirst(this)\"></div>";
+    }
+
+    function Creatediv() {
+        for (a; a < tablepop.length; a++) {
+            document.getElementById(a).innerHTML = "<div class =\"thumbnail text-center\"><h3>" + tablepop[a] + "</h3><img src =\"img/" + a + ".jpg \" alt =\"...\"><br></div></div>";
+        }
+    }
+    Creatediv();
+}
+
+function isReady() {
+    if (ready == 1) {
+        document.getElementById("player1").innerHTML = "<div class =\"thumbnail text-center\"><br><img src =\"img/" + player1 + ".jpg \" alt =\"...\"><div class=\"caption\"><p class=\"btn btn-success btn-lg btn-block\" type=\"submit\" role=\"button\" onclick=\"isReady()\">Player 1 is ready</p></div>";
+        ready = 2;
+
+    } else if (ready == 2) {
+        document.getElementById("player2").innerHTML = "<div class =\"thumbnail text-center\"><br><img src =\"img/" + player2 + ".jpg \" alt =\"...\"><div class=\"caption\"><p class=\"btn btn-success btn-lg btn-block\" type=\"submit\" role=\"button\" onclick=\"isReady()\">Player 2 is ready</p></div>";
+        ready = 3;
+        document.getElementById("start").innerHTML = " <br><a href=\"battle.html\"><p  class =\"btn btn-success btn-lg btn-block \" role = \"button\">FIGHT</p></a> </div > ";
+
+
+    }
+}
+
+function selectFirst(that) {
+
+    if (ready == 1) {
+        player1 = that.id;
+        document.getElementById("player1").innerHTML = "<div class =\"thumbnail text-center\"><br><img src =\"img/" + player1 + ".jpg \" alt =\"...\"><div class=\"caption\"><p class=\"btn btn-danger btn-lg btn-block\" type=\"submit\" role=\"button\" onclick=\"isReady()\">Pret ?</p></div>";
+        document.getElementById("readyPlayer1").innerHTML = "<h2>" + tablepop[player1] + "</h2>";
+        localStorage.setItem("player1Name", tablepop[player1]);
+        localStorage.setItem("player1id", player1);
+
+
+        return player1;
+    } else if (ready == 2) {
+        player2 = that.id;
+
+        document.getElementById("player2").innerHTML = "<div class =\"thumbnail text-center\"><br><img src =\"img/" + player2 + ".jpg \" alt =\"...\"><div class=\"caption\"><p class=\"btn btn-danger btn-lg btn-block\" type=\"submit\" role=\"button\" onclick=\"isReady()\">Pret ?</p></div>";
+        document.getElementById("readyPlayer2").innerHTML = "<h2>" + tablepop[player2] + "</h2>";
+        localStorage.setItem("player2Name", tablepop[player2]);
+        localStorage.setItem("player2id", player2);
+
+        return player2;
+    }
+}
